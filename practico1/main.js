97248e9011b658867f7a11a1a6db4c51949131c5
lourdes-miazzo <lourdesmiazzo@gmail.com>
2023-03-13 11:54:53 -0300

    mini correccion

--- practico1/main.js
+++ practico1/main.js
@@ -36,7 +36,7 @@ class ProductManager{
    getProductById(prodId){
         const product = this.products.find(prod=> prod.id === prodId)
         if(!product){
-            throw Error (`Product with ID:${prodId} not found`)
+            return `Product with ID:${prodId} not found`
         }else{
             return product
         }
@@ -45,9 +45,11 @@ class ProductManager{
 
 const productosNuevos = new ProductManager()
 
+console.log(productosNuevos.getProducts())
 console.log(productosNuevos.addProduct("Manzana", "colorada, de Rio Negro", 500, "images/frutas/manzana.jpg","jkl122", 890))
 console.log(productosNuevos.addProduct("Pan Integral", "al horno con semillas", 790,"images/panificadps/panSemillas", "jkl122", 950))
 console.log(productosNuevos.addProduct("Pera", "salteña", 640,"images/pera", "jkl123"))
 console.log(productosNuevos.addProduct("Pan blanco", "al horno con agregado de vitaminas", 1260,"images/panificados/panBlanco", "jkl124", 950))
 console.log(productosNuevos.getProducts())
 console.log(productosNuevos.getProductById(6))
+console.log(productosNuevos.getProductById(2))
